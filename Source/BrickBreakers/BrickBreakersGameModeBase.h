6d094a601717f13849b02f57778f6c1ef8f62283
Karim William <karim.william7@gmail.com>
2023-07-26 16:32:02 +0300

    Complete game push

---
+++ Source/BrickBreakers/BrickBreakersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BrickBreakersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRICKBREAKERS_API ABrickBreakersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
